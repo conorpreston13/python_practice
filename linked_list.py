e0dcaa3a079c65e1a550ff78e64342b7e6d258e8
Conor Preston <52896948+conorpreston13@users.noreply.github.com>
2020-04-27 10:19:13 -0700

    Add files via upload
    adds linked list

---
+++ linked_list.py
@@ -0,0 +1,67 @@
+#  LINKED LIST =================================================================
+
+class Node:
+
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+
+    def __str__(self):
+        return f"{self.data}"
+
+class LinkedList:
+
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def append_val(self, x):
+        '''add x to the end of the list'''
+        if not isinstance(x, Node):
+            x = Node(x)
+        if self.head == None:
+            self.head = x
+        else:
+            self.tail.next = x
+        self.tail = x
+
+    def __str__(self):
+        to_print = ""
+        current = self.head
+        while current is not None:
+            to_print += str(current.data) + "->"
+            current = current.next
+        if to_print:
+            return "[" + to_print[:-2] + "]"
+        return '[]'
+
+    def reverse_list_recur(self, current, previous):
+        '''reverse the sequence of node pointers'''
+        if self.head == None:
+            return
+        elif current.next == None:
+            self.tail = self.head
+            current.next = previous
+            self.head = current
+        else:
+            next = current.next
+            current.next = previous
+            self.reverse_list_recur(next, current)
+
+node1 = Node(1)
+node2 = Node(2)
+node3 = Node(3)
+node4 = Node(4)
+node5 = Node(5)
+
+my_list = LinkedList()
+my_list.append_val(node1)
+my_list.append_val(node2)
+my_list.append_val(node3)
+my_list.append_val(node4)
+my_list.append_val(node5)
+
+print(my_list)
+
+my_list.reverse_list_recur(my_list.head, None)
+print(my_list)
